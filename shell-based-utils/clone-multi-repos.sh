d301af96f56f4dfe5b673d7791add8d8cc19e1d0
shaunchokshi-gh <shaunchokshi-gh@proton.me>
2026-01-16 14:18:00 -0500

    added script to clone all repos from a given user/org

---
+++ shell-based-utils/clone-multi-repos.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+cntx="users"
+read -e -p "Context of repo ownership [orgs] or [users]:" cntx
+
+if [ -z "$1" ]; then
+    read -e -p "Name (of user or org):" name
+else
+    name=$1
+fi
+
+if [ -z "$2" ]; then
+    max=2
+    read -e -p "Maximum number of repos list pages to traverse (at 100 repos per page):" max
+else
+    max=$2
+fi
+page=1
+
+
+echo $name
+echo $max
+echo $cntx
+echo $page
+
+until (( $page -lt $max ))
+do
+    curl "https://api.github.com/$cntx/$name/repos?page=$page&per_page=100" | grep -e 'clone_url*' | cut -d \" -f 4 | xargs -L1 git clone
+    $page=$page+1
+done
+
+exit 0
